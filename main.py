b762d3e172727adca6bce413d31258d14d7b8173
Sushovit204 <shahsushovit434@gmail.com>
2022-10-09 22:50:56 +0545

    added the get request for post  of {id}

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randrange
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
@@ -11,6 +12,14 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int]
 
+my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1}, 
+{"title":"favourite food", "content":"I like pizza", "id":2}]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 @app.get("/")
 def root():
@@ -18,10 +27,17 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"get":"This is a post"}
+    return {"data":my_posts}
 
 @app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.dict())
-    return {"data": post}
+    post_dict= post.dict()
+    post_dict['id']=randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id):
+    post = find_post(int(id))
+    return {"post detail": post}
