21742bdf255fe2df2e216c6b61ddc598667d3f07
Sushovit204 <shahsushovit434@gmail.com>
2022-10-10 21:13:46 +0545

    completed the basic CRUD approach

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from random import randrange
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
+from itsdangerous import NoneAlgorithm
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -21,6 +22,12 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -29,7 +36,7 @@ def root():
 def get_posts():
     return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict= post.dict()
     post_dict['id']=randrange(0, 10000000)
@@ -37,7 +44,34 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 
-@app.get("/posts/{id}")
-def get_post(id):
-    post = find_post(int(id))
+@app.get("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id:{id} wasnot found")
     return {"post detail": post}
+
+
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"the post with id:{id} doesn't exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post:Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"the post with id:{id} doesn't exist")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
