8d18e9dccfcd20b3daab6aa13c17974b3ec5932d
Sushovit204 <shahsushovit434@gmail.com>
2022-10-14 15:11:37 +0545

    Made DB connection for deleting and getting posts of id

--- app/main.py
+++ app/main.py
@@ -56,28 +56,38 @@ def create_posts(post: Post):
     cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING *
                     """,
                     (post.title, post.content, post.published))
+
     new_post = cursor.fetchone()
+
     conn.commit()
+
     return {"data": new_post}
 
 
-@app.get("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id:{id} wasnot found")
-    return {"post detail": post}
+    return{"post_detail":post}
 
 
-@app.delete("/posts/{id}")
+@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
 
-    if index == None:
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+
+    conn.commit()
+    
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"the post with id:{id} doesn't exist")
-    my_posts.pop(index)
+
+    
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
