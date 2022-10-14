3edd16d23d7a523ffe8a098162ca4c06df1c0a33
Sushovit204 <shahsushovit434@gmail.com>
2022-10-14 21:09:09 +0545

    Setting up for sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 from random import randrange
 from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from itsdangerous import NoneAlgorithm
 from pydantic import BaseModel
 import psycopg2, time
 from psycopg2.extras import RealDictCursor
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 class Post(BaseModel):
     title: str
@@ -26,24 +32,14 @@ while True:
         print("Error :", error)
         time.sleep(2)
 
-my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1}, 
-{"title":"favourite food", "content":"I like pizza", "id":2}]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i,p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status":"success"}
+
 
 @app.get("/posts")
 def get_posts():
