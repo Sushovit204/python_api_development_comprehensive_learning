0f06b3594342c76e483155abefdca32a3d4e898a
Sushovit204 <shahsushovit434@gmail.com>
2022-10-15 20:47:16 +0545

    switching to sqlachemy from postgresql

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from email.policy import default
 from tokenize import String
 from xmlrpc.client import Boolean
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import null 
+from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import null, text
 from .database import Base
 
 class Post(Base):
@@ -11,4 +11,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable = False)
     content = Column(String, nullable = False)
-    published = Column(Boolean, default= True)
+    published = Column(Boolean, server_default ='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default = text('now()'))
