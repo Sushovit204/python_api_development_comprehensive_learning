e455a7192320ed1e10299af7eb6c83fc729355a7
Sushovit204 <shahsushovit434@gmail.com>
2022-10-18 21:10:17 +0545

    added the pw hashing and get user id

--- app/main.py
+++ app/main.py
@@ -122,6 +122,15 @@ def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     return new_user
 
+@app.get("/users/{id}", response_model= schemas.UserResponse)
+def get_user(id : int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"user with id: {id} does not exist")
+
+    return user                       
 
 
 
