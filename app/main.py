cde8bbb9ac78dd6460e2e30ccaba8441f06859f3
Sushovit204 <shahsushovit434@gmail.com>
2022-10-21 18:15:11 +0545

    login and authorization

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from psycopg2.extras import RealDictCursor
 from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -31,6 +31,8 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
+
 
 
 @app.get("/")
